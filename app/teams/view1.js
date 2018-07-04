c212ab6bf94a6c834a911c356d6ff06432ed96b5
Adam <adamvermes@outlook.com>
2018-07-04 10:06:32 +0200

    basic css added

--- app/teams/view1.js
+++ app/teams/view1.js
@@ -33,39 +33,14 @@ view1Controller.controller('View1Ctrl', function($scope, $http) {
         }
         generateTeams(16);
 
-        function fillPlayers() {
-            var slicedArray = [];
-            for (let teamMember = 0, teamIndex = 0; teamMember < playerData.length; teamMember+=11, teamIndex++) {
-                slicedArray = playerData.results.slice(teamMember, teamMember+11);
-                teams[teamIndex].players = slicedArray;
-            }
-        }
-        fillPlayers();
-        console.log(teams);
     });
 });
-//
-// let teams = [];
-// var teamNames = ["France", "Argentina", "Brazil", "Croatia", "Spain", "England",
-//     "Belgium", "Hungary", "Japan", "Columbia", "Mexico", "Germany", "Serbia",
-//     "Australia", "Denmark", "Iceland", "Portugal"];
-//
-// function generateTeams(teamNumbers) {
-//     for (let i = 1; i <= teamNumbers ; i++) {
-//         let team = {teamName: teamNames[i],
-//             players: []
-//         }
-//         teams.push(team);
+
+// function fillPlayers() {
+//     var slicedArray = [];
+//     for (let teamMember = 0, teamIndex = 0; teamMember < $scope.players.length; teamMember+=11, teamIndex++) {
+//         slicedArray = $scope.players.results.slice(teamMember, teamMember+11);
+//         teams[teamIndex].players = slicedArray;
 //     }
 // }
-// generateTeams(16);
-// console.log(teams);
-//
-// // function fillPlayers() {
-// //     var slicedArray = [];
-// //     for (let teamMember = 0, teamIndex = 0; teamMember < $scope.players.length; teamMember+=11, teamIndex++) {
-// //         slicedArray = $scope.players.results.slice(teamMember, teamMember+11);
-// //         teams[teamIndex].players = slicedArray;
-// //     }
-// // }
-// // fillPlayers();
+// fillPlayers();
