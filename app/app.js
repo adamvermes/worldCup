d40e131f74b57ee8bdfb3426bcad3a0719db4f56
Adam <adamvermes@outlook.com>
2018-07-10 22:27:03 +0200

    <h2> design

--- app/app.js
+++ app/app.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'teamController',
-   'scheduleController',
-  'myApp.version'
-])
-.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+angular
+    .module('myApp', [
+        'ngRoute',
+        'teamController',
+         'scheduleController',
+         'myApp.version'
+    ])
+    .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
 
-  $routeProvider.otherwise({redirectTo: '/worldCup'});
-}]);
+        $routeProvider.otherwise({redirectTo: '/worldCup'});
+    }]);
